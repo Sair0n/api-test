e66e3276e1eee0b0e4cb011ab744816daded5728
Sairon <sairon@example.com>
2022-12-16 17:26:48 +0300

    Validation

--- app/Http/Controllers/Api/TopPosController.php
+++ app/Http/Controllers/Api/TopPosController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiRequest;
 use Illuminate\Http\Request;
 use App\Models\TopPos;
 
 class TopPosController extends Controller
 {
-    //
+    public function index(ApiRequest $request)
+    {
+
+    }
 }
