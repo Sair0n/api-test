78fdcdbb21e059a5cd1bb7cc48c64eb9f7d4c1cc
Sairon <sairon@example.com>
2022-12-18 02:54:36 +0300

    collect:top-history

--- app/Console/Commands/CollectTopHistory.php
+++ app/Console/Commands/CollectTopHistory.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\TopPos;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
 
 class CollectTopHistory extends Command
 {
@@ -11,32 +15,80 @@ class CollectTopHistory extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'collect:top-history {dateFrom} {dateTo?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Collect Application Top Category Positions from apptica.com';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        return 0;
+        if ($this->validateInput($this->argument('dateFrom'))){
+            $url = 'https://api.apptica.com/package/top_history/1421444/1?date_from=' . $this->argument('dateFrom') . '&date_to=';
+        } else {
+            return;
+        }
+
+        if($this->argument('dateTo')){
+            if ($this->validateInput($this->argument('dateTo'))){
+                $url .=  $this->argument('dateTo') . '&B4NKGg=fVN5Q9KVOlOHDx9mOsKPAQsFBlEhBOwguLkNEDTZvKzJzT3l';
+            } else {
+                return;
+            }
+        } else {
+            $url .=  $this->argument('dateFrom') . '&B4NKGg=fVN5Q9KVOlOHDx9mOsKPAQsFBlEhBOwguLkNEDTZvKzJzT3l';
+        }
+
+
+        $apiResponse = Http::get($url);
+        if($apiResponse['status_code'] == 200){
+            foreach ($apiResponse['data'] as $cat => $value) {
+
+                $period = [];
+
+                foreach ($value as $subcat => $v) {
+
+                    foreach ($v as $day => $pos) {
+                        if (is_null($pos)) continue;
+                        if (empty($period[$day])){
+                            $period[$day] = $pos;
+                        }
+                        if ($period[$day] > $pos){
+                            $period[$day] = $pos;
+                        }
+                        TopPos::firstOrCreate(['category' => $subcat, 'parentCategory' => $cat, 'position' => $pos, 'date' => $day]);
+                    }
+                }
+
+                foreach ($period as $day => $position) {
+                    if (is_null($position)) continue;
+                    TopPos::firstOrCreate(['category' => $cat, 'parentCategory' => NULL, 'position' => $position, 'date' => $day]);
+                }
+
+            }
+        } else {
+            $this->error('Request failed');
+            return;
+        }
+
+
+        $this->info('Success ' . $this->argument('dateFrom') . ' - ' .  $this->argument('dateTo'));
     }
+
+
+    public function validateInput($value)
+    {
+        $validator = Validator::make(['date' => $value], ['date' => 'required|date_format:Y-m-d']);
+        if ($validator->fails()) {
+            $this->error($validator->errors());
+            return false;
+        }else{
+            return true;
+        }
+    }
+
 }
